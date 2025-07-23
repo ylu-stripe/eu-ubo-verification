c231db5803e15fa1b518ab00847a2aa0ff158ab6
Yueling Lu <ylu@stripe.com>
2025-07-23 14:43:13 -0400

    Fix React Router basename for GitHub Pages deployment

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/eu-ubo-verification">
       <App />
     </BrowserRouter>
   </React.StrictMode>
